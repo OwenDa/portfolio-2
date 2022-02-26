7625223738e8fcf8bd8d93b755afca2b1ac41bd9
OwenDa <owendarcy25@gmail.com>
2022-02-26 15:08:44 +0000

    Add user's max + threshold inputs to setParameters function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,21 @@
 let parameters = document.getElementById('parameters');
 parameters.addEventListener('submit', setParameters);
 
+/** Gets user's form input and assigns those values to the appropriate variables.
+ */
 function setParameters(event){
     event.preventDefault();
     let unitInput = document.getElementById('unit-input').value;
     unitDisplay.innerHTML = unitInput;
+    maxCapacity = parseInt(document.getElementById('max-input').value);
+    threshold = parseInt(document.getElementById('threshold-input').value);
+    // For testing:
+    console.log(unitDisplay);
+    console.log(maxCapacity);
+    console.log(threshold);
 }
 
-// Test element identification (count, units, message):
+// Counter Variables
 let currentQty = 90;
 let int = document.getElementById('count');
 int.innerHTML = currentQty;
