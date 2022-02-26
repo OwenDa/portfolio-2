0c5fae0d9bfab38800871305f32d0a5111c3580f
OwenDa <owendarcy25@gmail.com>
2022-02-26 10:14:00 +0000

    Begin connecting JS variables to HTML elements

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// Test element identification (count, units):
+let currentQty = 90;
+let int = document.getElementById('count');
+int.innerHTML = currentQty;
+
+let units = document.getElementById('units');
+units.innerHTML = "examples";
