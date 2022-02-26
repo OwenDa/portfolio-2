772976ee07de2cf1fb2207aabd8add2a777041cb
OwenDa <owendarcy25@gmail.com>
2022-02-26 10:48:55 +0000

    Add +/- buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
-// Test element identification (count, units):
+// Test element identification (count, units, message):
 let currentQty = 90;
 let int = document.getElementById('count');
 int.innerHTML = currentQty;
 
 let units = document.getElementById('units');
 units.innerHTML = "examples";
+
+let message = document.getElementById('message');
+message.textContent = "Example message here."
