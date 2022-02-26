7c8a9222b7d66a684d9b2d4a75de40e81e98b340
OwenDa <owendarcy25@gmail.com>
2022-02-26 12:16:15 +0000

    Add +/- button functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,24 @@ units.innerHTML = "examples";
 
 let message = document.getElementById('message');
 message.textContent = "Example message here."
+
+// Identify Buttons
+let plusButton = document.getElementById('plus');
+let minusButton = document.getElementById('minus');
+
+/** Increases the currentQty and updates the count accordingly */
+function increase() {
+    currentQty += 1;
+    int.innerHTML = currentQty;
+}
+
+/** Decreases the currentQty and updates the count accordingly */
+function decrease() {
+    currentQty -= 1;
+    int.innerHTML = currentQty;
+}
+
+
+// EVENT LISTENERS
+plusButton.addEventListener('click', increase);
+minusButton.addEventListener('click', decrease);
